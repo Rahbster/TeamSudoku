2514f986836f4cc5de1f1fbe601fa11a1478594f
Rahbster <rahbster@yahoo.com>
2025-09-06 23:41:33 -0400

    Moving sw.js

--- js/scripts.js
+++ js/scripts.js
@@ -162,7 +162,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/js/sw.js')
+        navigator.serviceWorker.register('/sw.js')
             .then((registration) => {
                 console.log('Service Worker registered! Scope: ', registration.scope);
             })
